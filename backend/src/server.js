45e7995baaff9e8f9c1f6128c707240c603f097b
LogicalGagan <gagangn9148@gmail.com>
2025-09-20 17:46:40 +0530

    prepared for deeployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,17 +4,35 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv"
 import { rateLimiterMiddleware } from "./middleware/ratelimiter.js";
 import cors from "cors"
+import path from "path"
 dotenv.config();
-const app =express();
+
+const __dirname = path.resolve();
+const app = express();
+
 
 connectDB();
 
 app.use(express.json());
 
-app.use(cors({origin :"http://localhost:5173"}))
+if (process.env.NODE_ENV !== "production") {
+
+    app.use(cors({ origin: "http://localhost:5173" }))
+}
 app.use("/api/notes", rateLimiterMiddleware, notesRoutes);
 
-const PORT =process.env.PORT;
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+
+
+    })
+}
+
+
+
+const PORT = process.env.PORT;
 
 
 app.listen(PORT);
